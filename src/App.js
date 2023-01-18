17cb0c3a5a1361d0ef82fce4b1053801fe99598c
Conor Higgins <wodowods@gmail.com>
2023-01-18 22:57:32 +0000

    lerp test using coordinate array and state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import Sphere from "./Components/Sphere/Sphere.jsx";
 import { Canvas, useFrame } from "react-three-fiber";
+import React, { useRef, useState, useMemo } from "react";
 
 function App() {
+    const coords = [0.1, 0.4, 0.3, 0.2, 1, 2, 0.5, 0.1, 0.05]; /// TODO: This will be a frequency heightmap from an audio file
+    const [index, setIndex] = useState(0);
+    const [currentX, setCurrentX] = useState(coords[index]);
     return (
-        <Canvas dpr={[1, 2]} camera={{ fov: 25 }}>
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 10, 5]} />
-            <pointLight position={[-10, -10, -5]} />
-            <Sphere />
-        </Canvas>
+        <>
+            <button
+                onClick={() => {
+                    index >= coords.length - 1 ? setIndex(0) : setIndex(index + 1);
+                    setCurrentX(coords[index]);
+                }}
+            >
+                Current X{" "}
+            </button>
+            <Canvas dpr={[1, 2]} camera={{ fov: 25 }}>
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 5]} />
+                <pointLight position={[-10, -10, -5]} />
+                <Sphere currentX={currentX} />
+            </Canvas>
+        </>
     );
 }
 
--- src/Components/Sphere/Sphere.jsx
+++ src/Components/Sphere/Sphere.jsx
@@ -2,23 +2,27 @@ import React, { useRef, useState, useMemo } from "react";
 
 import { Canvas, useFrame } from "react-three-fiber";
 import * as THREE from "three";
-import { sphereGeometry, meshStandardMaterial } from "three";
 
-const Sphere = () => {
+const Sphere = ({ currentX }) => {
     const ref = useRef();
+    const vecX = new THREE.Vector3(currentX, 0, currentX * -1);
+
     useFrame((state) => {
-        ref.current.rotation.set(ref.current.rotation.x + 0.1, ref.current.rotation.y + 0.1, ref.current.rotation.z + 0.1);
-        ref.current.scale.set(
-            Math.sin(state.clock.getElapsedTime() / 1.5) / 2,
-            Math.cos(state.clock.getElapsedTime() / 1.5) / 2,
-            Math.cos(state.clock.getElapsedTime() / 1.5) / 2
-        );
+        //ref.current.rotation.set(ref.current.rotation.x + 0.01, ref.current.rotation.y + 0.01, ref.current.rotation.z + 0.01);
+        /*ref.current.position.set(
+            coords[i], //Math.sin(state.clock.getElapsedTime() / coords[i]) / 2,
+            coords[i], //Math.cos(state.clock.getElapsedTime() / coords[i]) / 10,
+            coords[i] //Math.cos(state.clock.getElapsedTime() / coords[i]) / 2 + 0.05
+        );*/
+        ref.current.position.lerp(vecX, 0.1); /// This will watch for changes to the props in the vector and lerp accordingly
     });
     return (
-        <mesh ref={ref} visible userData={{ test: "here" }} position={[0, 0, 0]} castShadow>
-            <sphereGeometry attach="geometry" args={[0.5, 32, 32]} />
-            <meshStandardMaterial attach="material" color="blue" roughness={0.5} metalness={0.1} />
-        </mesh>
+        <>
+            <mesh ref={ref} visible userData={{ test: "here" }} position={[0, 0, 0]} castShadow>
+                <sphereGeometry attach="geometry" args={[0.5, 32, 32]} />
+                <meshStandardMaterial attach="material" color="blue" roughness={0.5} metalness={1} />
+            </mesh>
+        </>
     );
 };
 
